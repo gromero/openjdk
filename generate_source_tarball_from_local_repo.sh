f9934fa3ceadc74380660e904e2fc1c5f5ca6ce5
Gustavo Romero <gromero@br.ibm.com>
2016-06-23 17:19:07 -0300

    Add script to generate tarball from a local Hg repo

---
+++ generate_source_tarball_from_local_repo.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# TODO:
+#	repo, tag as arguments
+#	build .tar.xz filename using variables
+
+
+repo=jdk9
+subrepos="corba hotspot jdk jaxws jaxp langtools nashorn"
+tag=`hg id -t --cwd ${repo}`
+
+echo "Extracting archives from Mercurial repo (tag: ${tag})..."
+hg archive -t tar ${repo}.tar --cwd ${repo}
+echo "> ${repo}..."
+for subrepo in ${subrepos}
+do
+  echo ">> ${subrepo}..."
+  hg archive -t tar ${subrepo}.tar --cwd ${repo}/${subrepo}
+done
+
+mkdir tarball
+pushd tarball
+
+echo "Populating tree..."
+
+cp ../${repo}/${repo}.tar .
+tar xf ${repo}.tar
+rm ${repo}.tar
+# mv ${repo} jdk9
+pushd jdk9
+
+for subrepo in ${subrepos} 
+do
+# mkdir ${subrepo}
+# pushd ${subrepo}
+  cp ../../${repo}/${subrepo}/${subrepo}.tar .
+  tar xf ${subrepo}.tar
+  rm ${subrepo}.tar
+# popd
+done
+
+
+if [ -e jdk ]
+then # remove elliptic-curve ciphers, not supported on RHEL 7.2.
+  echo "Removing ECC..."
+  rm -vr jdk/src/jdk.crypto.ec/share/native/libsunec/impl
+fi
+
+popd
+
+echo "Generating tarball..."
+tar cJf "jdk9-${tag}.tar.xz" jdk9
+mv "jdk9-${tag}.tar.xz" ../
+
+popd
+
+rm -fr tarball
