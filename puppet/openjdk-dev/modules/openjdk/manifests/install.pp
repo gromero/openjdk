db7e1645a86279619796f14fc13bb12a76dd25de
Gustavo Romero <gusbromero@gmail.com>
2016-10-14 17:30:41 -0400

    Install -source besides -dbg packages

--- puppet/openjdk-dev/modules/openjdk/manifests/install.pp
+++ puppet/openjdk-dev/modules/openjdk/manifests/install.pp
@@ -1,7 +1,10 @@
 class openjdk::install {
 
 
-  package { [ 'libc6-dbg', 'vim', 'nmap' ]: 
+# Just remember that even with -source and -dbg files you also need to do a 'dir'
+# from gdb ;-)
+
+  package { [ 'libc6-dbg', 'glibc-source', 'vim', 'nmap' ]: 
     ensure => 'present',
   }
 }
