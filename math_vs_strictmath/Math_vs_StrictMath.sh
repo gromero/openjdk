d2886012a142d05846e1f1bf1bbbb2912d97a2a2
Gustavo Romero <gusbromero@gmail.com>
2016-10-17 16:16:15 -0400

    Add JAVA_HOME as a parameter

--- math_vs_strictmath/Math_vs_StrictMath.sh
+++ math_vs_strictmath/Math_vs_StrictMath.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+if [ ! -z $1 ]
+then
+  JAVA_PATH=$1
+
+if [ ! -f ${JAVA_PATH}/java ]
+then
+  echo "No Java runtime executable found! Exiting..."
+  exit 1
+fi
+
+if [ ! -f ${JAVA_PATH}/javac ]
+then
+  echo "No Java compiler found! Exiting..."
+  exit 1
+fi
+
+fi
+
 ####################
 # One arg functions.
 for FUNCTION in sin cos tan asin acos atan exp log log10 sqrt cbrt sinh cosh tanh expm1 log1p
@@ -12,11 +30,11 @@ do
   sed "s|FUNC|${FUNCTION}|" Math_FUNC.java       > ${MClassname}.java
   sed "s|FUNC|${FUNCTION}|" StrictMath_FUNC.java > ${SMClassname}.java
 
-  javac ${MClassname}.java
-  javac ${SMClassname}.java
+  ${JAVA_PATH}/javac ${MClassname}.java
+  ${JAVA_PATH}/javac ${SMClassname}.java
 
-  MATH_REALTIME=$({ time java ${MClassname} ; } |& fgrep real | cut -f2)
-  STRICT_MATH_REALTIME=$({ time java ${SMClassname} ; } |& fgrep real | cut -f2)
+  MATH_REALTIME=$({ time ${JAVA_PATH}/java ${MClassname} ; } |& fgrep real | cut -f2)
+  STRICT_MATH_REALTIME=$({ time ${JAVA_PATH}/java ${SMClassname} ; } |& fgrep real | cut -f2)
   STAT_OUTPUT="${FUNCTION} ${MATH_REALTIME} ${STRICT_MATH_REALTIME}"
   echo ${STAT_OUTPUT}
 
@@ -35,11 +53,11 @@ do
   sed "s|FUNC|${FUNCTION}|" Math_FUNC_2args.java       > ${MClassname}.java
   sed "s|FUNC|${FUNCTION}|" StrictMath_FUNC_2args.java > ${SMClassname}.java
 
-  javac ${MClassname}.java
-  javac ${SMClassname}.java
+  ${JAVA_PATH}/javac ${MClassname}.java
+  ${JAVA_PATH}/javac ${SMClassname}.java
 
-  MATH_REALTIME=$({ time java ${MClassname} ; } |& fgrep real | cut -f2)
-  STRICT_MATH_REALTIME=$({ time java ${SMClassname} ; } |& fgrep real | cut -f2)
+  MATH_REALTIME=$({ time ${JAVA_PATH}/java ${MClassname} ; } |& fgrep real | cut -f2)
+  STRICT_MATH_REALTIME=$({ time ${JAVA_PATH}/java ${SMClassname} ; } |& fgrep real | cut -f2)
   STAT_OUTPUT="${FUNCTION} ${MATH_REALTIME} ${STRICT_MATH_REALTIME}"
   echo ${STAT_OUTPUT}
 
