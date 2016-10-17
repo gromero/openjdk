2db98495f64624445b14edcbeadb16d3fe5f015b
Gustavo Romero <gromero@br.ibm.com>
2016-10-17 19:40:08 -0200

    Allow java and javac dir to be specified

--- math_vs_strictmath/Math_vs_StrictMath.sh
+++ math_vs_strictmath/Math_vs_StrictMath.sh
@@ -2,21 +2,32 @@
 
 if [ ! -z $1 ]
 then
-  JAVA_PATH=$1
+  JAVA_BINARIES=$1
 
-if [ ! -f ${JAVA_PATH}/java ]
-then
-  echo "No Java runtime executable found! Exiting..."
-  exit 1
+  if [ ! -f ${JAVA_BINARIES}/java ]
+  then
+    echo "No Java runtime executable found! Exiting..."
+    exit 1
+  fi
+
+  if [ ! -f ${JAVA_BINARIES}/javac ]
+  then
+    echo "No Java compiler found! Exiting..."
+    exit 1
+  fi
+
+  # Use specified java/javac.
+  JAVA=${JAVA_BINARIES}/java
+  JAVAC=${JAVA_BINARIES}/javac
+
+else
+# Use java/javac from environment.
+JAVA=java
+JAVAC=javac
+
 fi
 
-if [ ! -f ${JAVA_PATH}/javac ]
-then
-  echo "No Java compiler found! Exiting..."
-  exit 1
-fi
 
-fi
 
 ####################
 # One arg functions.
@@ -30,11 +41,11 @@ do
   sed "s|FUNC|${FUNCTION}|" Math_FUNC.java       > ${MClassname}.java
   sed "s|FUNC|${FUNCTION}|" StrictMath_FUNC.java > ${SMClassname}.java
 
-  ${JAVA_PATH}/javac ${MClassname}.java
-  ${JAVA_PATH}/javac ${SMClassname}.java
+  ${JAVAC} ${MClassname}.java  # Compile Math method
+  ${JAVAC} ${SMClassname}.java # Compile StrictMath method
 
-  MATH_REALTIME=$({ time ${JAVA_PATH}/java ${MClassname} ; } |& fgrep real | cut -f2)
-  STRICT_MATH_REALTIME=$({ time ${JAVA_PATH}/java ${SMClassname} ; } |& fgrep real | cut -f2)
+  MATH_REALTIME=$({ time ${JAVA} ${MClassname} ; } |& fgrep real | cut -f2)
+  STRICT_MATH_REALTIME=$({ time ${JAVA} ${SMClassname} ; } |& fgrep real | cut -f2)
   STAT_OUTPUT="${FUNCTION} ${MATH_REALTIME} ${STRICT_MATH_REALTIME}"
   echo ${STAT_OUTPUT}
 
@@ -53,11 +64,11 @@ do
   sed "s|FUNC|${FUNCTION}|" Math_FUNC_2args.java       > ${MClassname}.java
   sed "s|FUNC|${FUNCTION}|" StrictMath_FUNC_2args.java > ${SMClassname}.java
 
-  ${JAVA_PATH}/javac ${MClassname}.java
-  ${JAVA_PATH}/javac ${SMClassname}.java
+  ${JAVAC} ${MClassname}.java  # Compile Math method
+  ${JAVAC} ${SMClassname}.java # Compile StrictMath method
 
-  MATH_REALTIME=$({ time ${JAVA_PATH}/java ${MClassname} ; } |& fgrep real | cut -f2)
-  STRICT_MATH_REALTIME=$({ time ${JAVA_PATH}/java ${SMClassname} ; } |& fgrep real | cut -f2)
+  MATH_REALTIME=$({ time ${JAVA} ${MClassname} ; } |& fgrep real | cut -f2)
+  STRICT_MATH_REALTIME=$({ time ${JAVA} ${SMClassname} ; } |& fgrep real | cut -f2)
   STAT_OUTPUT="${FUNCTION} ${MATH_REALTIME} ${STRICT_MATH_REALTIME}"
   echo ${STAT_OUTPUT}
 
